7a83eecbc62bddfaa04061b59a6da7b027438c2f
Carl Jones <carljones9411@gmail.com>
2022-05-18 14:39:49 +0100

    finished navbar, added functionality and mobile/tablet responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.scss";
+import logo from "../../assets/logo.svg";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#wgpt3">What is GPT3?</a>
+      </p>
+      <p>
+        <a href="#possibilty">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Libary</a>
+      </p>
+    </>
+  );
+};
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gtp3__navbar">
+      <div className="gtp3__navbar-links">
+        <div className="gtp3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gtp3__navbar-links_container">
+          <Menu />
+        </div>
+
+        <div className="gpt3__navbar-sign">
+          <p>Sign In</p>
+          <button type="button">Sign Up</button>
+        </div>
+        <div className="gpt3__navbar-menu">
+          {toggleMenu ? (
+            <RiCloseLine
+              color="fff"
+              size={27}
+              onClick={() => setToggleMenu(false)}
+            />
+          ) : (
+            <RiMenu3Line
+              color="fff"
+              size={27}
+              onClick={() => setToggleMenu(true)}
+            />
+          )}
+          {toggleMenu && (
+            <div className="gpt3__navbar-menu_container scale-up-center">
+              <div className="gpt3__navbar-menu_conatiner-links">
+                <Menu />
+                <div className="gpt3__navbar-menu_container-links-sign">
+                  <p>Sign In</p>
+                  <button type="button">Sign Up</button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
