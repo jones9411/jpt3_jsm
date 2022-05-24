12b08fe1bde621904fa818080e0413a5c1c0bc37
Carl Jones <carljones9411@gmail.com>
2022-05-25 00:11:14 +0100

    finished project, checking to make sure it works on all screen sizes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Menu = () => {
         <a href="#wgpt3">What is GPT3?</a>
       </p>
       <p>
-        <a href="#possibilty">Open AI</a>
+        <a href="#possibility">Open AI</a>
       </p>
       <p>
         <a href="#features">Case Studies</a>
